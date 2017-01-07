9fbadaf3b48d78a6c90613601f05c2bac94374e8
Radiant <ldh0227@gmail.com>
2017-01-07 22:43:00 +0900

    add pythonanywhere.com

--- BanManager/settings.py
+++ BanManager/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'r)9(0xj97oxwuq0cz43jg(z^d+8sj11@!1iz4k))e1qh$7df83'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['121.169.159.232']
+ALLOWED_HOSTS = [
+    '121.169.159.232',
+    'ldh0227.pythonanywhere.com'
+]
 
 
 # Application definition
